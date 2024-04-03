ea5bb3ed230d8ee50f65aba0006239454e31cc19
Patar23 <patrik.veres@student.ukf.sk>
2024-04-03 18:03:00 +0200

    header, footer

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset='utf-8'>
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-        <title>FC STATS</title>
+        <title>Hokej</title>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
         <link rel='stylesheet' type='text/css' media='screen' href='style/style.css'>
         <script src='main.js'></script>
@@ -14,7 +14,7 @@
     ?>
 
     <body>
-        <div style="padding:16px">
+        <div>
             <h2>Obsah stránky</h2>
             <p>Text stránky...</p>
         </div>
